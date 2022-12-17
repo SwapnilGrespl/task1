390d271a9614a2cd3fe070bf2be2b8d7e4aa14ef
SwapnilGrespl <120772063+SwapnilGrespl@users.noreply.github.com>
2022-12-17 13:33:44 +0530

    Initial Commit
    Initial Git committed Task Completed

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log('All the best swapnil');
+
+$(document).ready(function(){
+$('#menuIcon').click(function(){
+  $('#sideBar').show();
+});
+});
+
+$(document).ready(function(){
+    $('#closeButton').click(function(){
+      $('#sideBar').hide();
+    });
+    });
